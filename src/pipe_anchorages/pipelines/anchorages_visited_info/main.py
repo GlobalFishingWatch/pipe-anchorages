4ae42aba5edef0d98f6604f5fba7551aae3b02ce
Tomás Link <tomas.link@globalfishingwatch.org>
2025-12-15 12:46:59 -0300

    Use staging table + rename strategy

--- src/pipe_anchorages/pipelines/anchorages_visited_info/main.py
+++ src/pipe_anchorages/pipelines/anchorages_visited_info/main.py
@@ -1,7 +1,10 @@
 import logging
+from datetime import datetime
 from types import SimpleNamespace
 from typing import Callable
 from functools import cached_property
+
+from google.api_core.exceptions import NotFound, Conflict
 
 from gfw.common.bigquery.helper import BigQueryHelper
 from gfw.common.query import Query
@@ -14,6 +17,10 @@ from pipe_anchorages.pipelines.anchorages_visited_info.table_config import (
 )
 
 logger = logging.getLogger(__name__)
+
+RENAME_QUERY = "ALTER TABLE `{old}` RENAME TO `{new}`"
+SUFFIX_STAGING = "_staging_{id}"
+SUFFIX_BACKUP = "_backup"
 
 
 class AnchoragesVisitedInfoQuery(Query):
@@ -55,24 +62,47 @@ def run(
 
     bq = BigQueryHelper(bq_client_factory, dry_run=config.dry_run, project=config.project)
 
+    staging_id = datetime.utcnow().strftime("%Y%m%d_%H%M%S_%f")
+
+    staging_table_id = config.bq_output + SUFFIX_STAGING.format(id=staging_id)
+    backup_table_id = config.bq_output + SUFFIX_BACKUP
+    prod_table_id = config.bq_output
+
     table_config = AnchoragesVisitedInfoTableConfig(
-        table_id=config.bq_output,
+        table_id=staging_table_id,
         description=AnchoragesVisitedInfoTableDescription(
             version=__version__,
             relevant_params={}
         ),
     )
 
-    # This pattern is not good. If something fails afterwards, we are left with no data.
-    # But using WRITE_DISPOSITION=True removes the schema.
-    # And updating the schema afterwards fails because there are REQUIRED fields.
-    # TODO: Move to a pattern in which we write into a temporary table and then copy.
-    logger.info(f"Re-creating events table '{config.bq_output}'...")
-    bq.client.delete_table(config.bq_output, not_found_ok=True)
+    logger.info(f"Creating staging table '{staging_table_id}...")
     bq.create_table(**table_config.to_bigquery_params(), labels=config.labels)
 
     logger.info('Executing anchorages visited info query...')
-    query_result = bq.run_query(query.render(), config.bq_output, labels=config.labels)
-
+    query_result = bq.run_query(
+        query.render(),
+        staging_table_id,
+        labels=config.labels,
+        write_disposition="WRITE_APPEND",
+        create_disposition="CREATE_NEVER",
+    )
     _ = query_result.query_job.result()
+
+    logger.info(f"Renaming prod table to backup: {backup_table_id}...")
+    try:
+        query = RENAME_QUERY.format(old=prod_table_id, new=backup_table_id.split(".")[-1])
+        query_result = bq.run_query(query)
+        _ = query_result.query_job.result()
+    except (NotFound, Conflict):
+        # If prod doesn't exist, just skip
+        pass
+
+    logger.info("Renaming staging table to prod...")
+    query = RENAME_QUERY.format(old=staging_table_id, new=prod_table_id.split(".")[-1])
+    query_result = bq.run_query(query)
+    _ = query_result.query_job.result()
+
+    logger.info("Deleting backup table...")
+    bq.client.delete_table(backup_table_id, not_found_ok=True)
     logger.info("Done.")
